30a4543860115b51a5f65f3cba7c1a8c2bd14b07
Murillo Serrano <murillosfcosta@gmail.com>
2026-03-30 09:09:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,6 @@
-// tipos primitivos 
-// boolean
- var boolean = false;
+let num = parseInt(prompt("digite um numero: "))
+let num2 = parseInt(prompt("digite outro numero: "))
 
- var prof; 
- console.log("Texto "+ boolean + "texto ");
- // template string
-console .log(`A variável ${boolean} tem o tipo ${typeof(boolean)}`);
+alert (`A soma é ${num + num2}`)
+alert (`O produto é ${num * num2}`)
 
-var numero = 1;
-console .log(`A variável ${numero} tem o tipo ${typeof(numero)}`)
- 
-// var; // variavel global ou local - valor incial pode ser nulo
-// let; // variavel local de bloco - valor inicial pode ser nulo
-// const; // variavel local de bloco - valor inicial é obrigatório
-
-// var prof = 'Lucas';
-// var prof = 'Lucas sousa';
- 
-// left Sobrenome ='Sousa';
-
-// Sobrenome = ' Borges de Sousa';
-
-// const profissão = 'prfessor';
-
-// profissão = 'programador';
-
-// const profissão = 'testador';
-var nome = 'Lucas';
-
-// Usando variavel local
-function nomeDafuncao(){
-     var Sobrenome = 'Sousa';
-     console.log(Sobrenome)
-}
-
-console.log(nome);
-nomeDafuncao();
-
-// operadores de comparação
-var comparação = '0' == 0;
-console.log(comparação);
-
-var comparaçãoIdentica ='0' === 0;
-console.log(comparaçãoIdentica);
-
-var mult = 5 * 9;
-console.log(mult);
-
-var divisao = 15 / 3;
-console.log(divisao);
-
-// operadores relacionais
-// >, <, >=, <=, !=
-// maior ou igual
-var maiorOuigual = 5 >= 16;
-console.log(maiorOuigual);
-
-var diferente = 5 != 8;
-console.log(diferente);
